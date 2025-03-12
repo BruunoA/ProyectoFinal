7597be186734114ca164b7a68033b319b8d11b46
Harsh Kumar Saini <150950663+Culebrin19@users.noreply.github.com>
2025-03-12 19:53:21 +0100

    provaTernaries

---
+++ app/Database/Migrations/2025-03-12-184555_MenuTagsMigration.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class MenuTagsMigration extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_menu'          => [
+                'type'           => 'INT',
+            ],
+            'id_tags'          => [
+                'type'           => 'INT',
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id_menu', 'id_tags');
+        $this->forge->addForeignKey('id_menu', 'menu', 'id');
+        $this->forge->addForeignKey('id_tags', 'tags', 'id');
+        $this->forge->createTable('menu_tags');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('menu_tags');
+    }
+}
