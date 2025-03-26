75a2d060412a07494cf3888525857f0281c4f9f1
Harsh Kumar Saini <150950663+Culebrin19@users.noreply.github.com>
2025-03-26 18:39:27 +0100

    noticiesBBDD

--- app/Controllers/NoticiesController.php
+++ app/Controllers/NoticiesController.php
@@ -4,13 +4,16 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\GestioModel;
 
 class NoticiesController extends BaseController
 {
     public function index()
     {
         $model = new GestioModel();
-        $data['gestio'] = $model->findAll();
+        // agafa tot lo que té com a secció noticies
+        $data['gestio'] = $model->where('seccio', 'noticies')->findAll();
+        // $data['gestio'] = $model->findAll();
         return view('noticies', $data);
     }
 }
