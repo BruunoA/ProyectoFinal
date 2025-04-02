024ebd659dfa0e86b16927e176d6ba872c15a581
BruunoA <bandia@inscaparrella.cat>
2025-04-02 20:39:37 +0200

    banner carrusel

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\BannerModel;
 
 class Home extends BaseController
 {
     public function index(): string
     {
+
+        $bannerModel = new BannerModel();
+        $banners['img'] = $bannerModel->findAll();
         return view('pagina_inicial');
     }
 }
--- app/Views/home.php
+++ app/Views/home.php
@@ -18,9 +18,8 @@
 
 <?= $this->include('general/menu'); ?>
 
-    <!-- Carrusel Bootstrap -->
     <section class="w3-container w3-center w3-padding-32">
-    <div id="carouselExample" class="carousel slide w-75 mx-auto" data-bs-ride="carousel" data-bs-interval="4000">
+    <div id="carouselExample" class="carousel slide w-75 mx-auto" data-bs-ride="carousel" data-bs-interval="1000">
 
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -35,16 +34,15 @@
             </div>
 
         
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Anterior</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Siguiente</span>
-            </button>
+            </button> -->
 
-            <!-- Indicadores -->
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2"></button>
