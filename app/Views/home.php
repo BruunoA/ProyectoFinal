0676be9395a7fb14b89bdaca028a4a96a7ab3a19
BruunoA <bandia@inscaparrella.cat>
2025-05-08 17:27:52 +0200

    preuba

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,24 +4,20 @@
 <head>
     <title>Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <link rel="stylesheet" href="<?= base_url('assets/css/main.css'); ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/homecalendario.css'); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
 </head>
-<style>
+<!-- <style>
     .w3-card-4 .w3-container {
         flex-grow: 1;
         display: flex;
         flex-direction: column;
     }
-</style>
+</style> -->
 
 <body>
     <?= $this->include('general/menu'); ?>
