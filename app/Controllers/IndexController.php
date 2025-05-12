fd681d13e49e72c6dfdd6b6cb9ef30d755082ba9
Harsh Kumar Saini <150950663+Culebrin19@users.noreply.github.com>
2025-05-12 15:17:13 +0200

    seccionsInici

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -18,11 +18,11 @@ class IndexController extends BaseController
         $model = new GestioModel();
 
         $data = [
-            'noticies' => $model->where('seccio', 'noticies')->where('destacat', 1)->orderBy('created_at', 'desc')->findAll(6),
-            'banners' => $model->where('seccio', 'banner')->where('estat', 1)->findAll(4),
-            'missio' => $model->where('seccio', 'missio')->where('estat', 1)->first(),
-            'visio' => $model->where('seccio', 'visio')->where('estat', 1)->first(),
-            'banners' => $modelBanners->where('seccio', 'banner')->where('estat', 1)->findAll(3),
+            'noticies' => $model->where('id_seccio', 1)->where('destacat', 1)->orderBy('created_at', 'desc')->findAll(6),
+            // 'banners' => $model->where('seccio', 6)->where('estat', 1)->findAll(4),
+            'missio' => $model->where('id_seccio', 3)->where('estat', 1)->first(),
+            'visio' => $model->where('id_seccio', 4)->where('estat', 1)->first(),
+            'banners' => $modelBanners->where('id_seccio', 6)->where('estat', 1)->findAll(3),
         ];
 
         // dd($data['banners']);
