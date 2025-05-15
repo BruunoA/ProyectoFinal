10ca2831d092ce22ea06532af31fec805083a823
Harsh Kumar Saini <150950663+Culebrin19@users.noreply.github.com>
2025-05-15 02:01:33 +0200

    menuNormal

--- app/Helpers/menu_helper.php
+++ app/Helpers/menu_helper.php
@@ -3,21 +3,32 @@
 use App\Models\ConfiguracioModel;
 
 if (!function_exists('mostrar_tree')) {
-    function mostrar_tree ($categories) {
-        //mostrar categoria
+    function mostrar_tree()
+    {
         $model = new ConfiguracioModel();
-        echo "<ol>";
-     
-        foreach ($categories as $cat) {
-            echo "<li>" . $cat['nom'] . "</li>";
 
-            $fills = $model->get_fills($cat['id']);
-            
-            if (count($fills)>0 )
-                mostrar_tree($fills);
-        }
-        echo "</ol>";
+        $categories = $model->where('tipus', 'menu_general') ->where('visibilitat', 1)->orderBy('ordre', 'ASC')->findAll();
+
+        return build_menu_tree($categories);
     }
 }
 
+if (!function_exists('build_menu_tree')) {
+    function build_menu_tree(array $elements, int $parentId = 0)
+    {
+        $branch = [];
+
+        foreach ($elements as $element) {
+            if ((int)$element['id_pare'] === $parentId) {
+                $children = build_menu_tree($elements, (int)$element['id']);
+                if ($children) {
+                    $element['children'] = $children;
+                }
+                $branch[] = $element;
+            }
+        }
+
+        return $branch;
+    }
+}
 
