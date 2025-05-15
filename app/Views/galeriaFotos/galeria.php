5389bf55b86ea309cc6229f4041e3263ffad5fee
Harsh Kumar Saini <150950663+Culebrin19@users.noreply.github.com>
2025-05-16 01:11:38 +0200

    galeria

--- app/Views/galeriaFotos/galeria.php
+++ app/Views/galeriaFotos/galeria.php
@@ -9,25 +9,28 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/galeria.css'); ?>">
     <title><?= lang('galeria.Titol') ?></title>
     <style>
-.img-album {
-        height: 200px;
-        overflow: hidden;
-    }
+        .img-album {
+            height: 200px;
+            overflow: hidden;
+        }
 
-    .img-album img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        display: block;
-    }
+        .img-album img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            display: block;
+        }
     </style>
 </head>
 
 <body>
     <?= $this->include('general/menu'); ?>
     <?= session()->getFlashdata('error') ?>
-    <div class="cards-container w3-margin-bottom">
-        <h1><?= lang('galeria.TitolGaleria') ?></h1>
+    <div class="cards-container w3-margin-bottom w3-container" style="margin-top: 2rem;">
+        <div class="w3-container w3-blue" style="margin-bottom: 2rem">
+            <h1 class="w3-center w3-text-white"><?= lang('galeria.TitolGaleria') ?></h1>
+        </div>
+
         <?php if (!empty($albums)): ?>
             <div class="w3-row-padding">
                 <?php foreach ($albums as $albumId => $album): ?>
